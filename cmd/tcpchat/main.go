743e4ed1dbf502d5fef509aea1f8a216759c546e
Altabaev <altabaev2013@gmail.com>
2021-09-23 11:39:53 +0300

    Temporally ignore error

--- cmd/tcpchat/main.go
+++ cmd/tcpchat/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"github.com/BurntSushi/toml"
 	"github.com/sirupsen/logrus"
-	"log"
 	"tcp-chat/internal/app"
 )
 
@@ -24,8 +23,8 @@ func main() {
 	config := app.NewConfig()                  // инициализируем конфигурацию
 	_, err := toml.Decode(configPath, &config) // декодируем файл конфигурации в структуру
 	if err != nil {
-		log.Fatal(err.Error())
-		//logger.Info("Config file not found. Chat's been run with default settings.")
+		//log.Fatal(err.Error())
+		logger.Info("Config file not found. Chat's been run with default settings.")
 	}
 	// инициализируем и запускаем приложение
 	chat := app.New(config, logger)
